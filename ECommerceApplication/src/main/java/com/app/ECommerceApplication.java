db18b93a51dc6c3693b9c56ce73596489c57966f
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2026-02-14 20:10:45 +0700

    feat: seed relation category and address

--- ECommerceApplication/src/main/java/com/app/ECommerceApplication.java
+++ ECommerceApplication/src/main/java/com/app/ECommerceApplication.java
@@ -10,7 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.app.config.AppConstants;
+import com.app.entites.Address;
+import com.app.entites.Category;
 import com.app.entites.Role;
+import com.app.repositories.AddressRepo;
+import com.app.repositories.CategoryRepo;
 import com.app.repositories.RoleRepo;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
@@ -23,6 +27,12 @@ public class ECommerceApplication implements CommandLineRunner {
 
 	@Autowired
 	private RoleRepo roleRepo;
+
+	@Autowired
+	private CategoryRepo categoryRepo;
+
+	@Autowired
+	private AddressRepo addressRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
@@ -45,10 +55,39 @@ public class ECommerceApplication implements CommandLineRunner {
 			userRole.setRoleName("USER");
 
 			List<Role> roles = List.of(adminRole, userRole);
+			roleRepo.saveAll(roles);
 
-			List<Role> savedRoles = roleRepo.saveAll(roles);
+			if (categoryRepo.count() == 0) {
+				Category shoes = new Category();
+				shoes.setCategoryName("Shoes");
 
-			savedRoles.forEach(System.out::println);
+				Category socks = new Category();
+				socks.setCategoryName("Socks");
+
+				categoryRepo.saveAll(List.of(shoes, socks));
+			}
+
+			if (addressRepo.count() == 0) {
+				Address address1 = new Address();
+				address1.setCountry("Indonesia");
+				address1.setState("Jawa Barat");
+				address1.setCity("Bandung");
+				address1.setPincode("401111");
+				address1.setStreet("Main Street 1");
+				address1.setBuildingName("Block A");
+
+				Address address2 = new Address();
+				address2.setCountry("Indonesia");
+				address2.setState("DKI Jakarta");
+				address2.setCity("Jakarta");
+				address2.setPincode("102200");
+				address2.setStreet("Second Street 5");
+				address2.setBuildingName("Tower B");
+
+				addressRepo.saveAll(List.of(address1, address2));
+			}
+
+			System.out.println("Seed data initialized");
 
 		} catch (Exception e) {
 			e.printStackTrace();
